2ccdbea7cb8e5e0637e1d63f4d60aa86728b6348
Sharon-uzu <victorysharon0@gmail.com>
2024-10-29 10:17:03 +0100

    first commit

---
+++ script/main.js
@@ -0,0 +1,72 @@
+var mode1 = document.getElementById('mode1');
+var mode2 = document.getElementById('mode2');
+var mode3 = document.getElementById('mode3');
+var mode4 = document.getElementById('mode4');
+var mode5 = document.getElementById('mode5');
+var mode6 = document.getElementById('mode6');
+var mode7 = document.getElementById('mode7');
+var mode8 = document.getElementById('mode8');
+
+function m1(){
+    mode1.style.display= 'flex';
+}
+
+function m2(){
+    mode2.style.display= 'flex';
+}
+
+function m3(){
+    mode3.style.display= 'flex';
+}
+
+function m4(){
+    mode4.style.display= 'flex';
+}
+
+function m5(){
+    mode5.style.display= 'flex';
+}
+
+function m6(){
+    mode6.style.display= 'flex';
+}
+
+function m7(){
+    mode7.style.display= 'flex';
+}
+
+function m8(){
+    mode8.style.display= 'flex';
+}
+
+function x1(){
+    mode1.style.display= 'none';
+}
+
+function x2(){
+    mode2.style.display= 'none';
+}
+
+function x3(){
+    mode3.style.display= 'none';
+}
+
+function x4(){
+    mode4.style.display= 'none';
+}
+
+function x5(){
+    mode5.style.display= 'none';
+}
+
+function x6(){
+    mode6.style.display= 'none';
+}
+
+function x7(){
+    mode7.style.display= 'none';
+}
+
+function x8(){
+    mode8.style.display= 'none';
+}
